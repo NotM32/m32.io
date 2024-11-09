5b2670e73267c82051d0eabd2a9da5b563ca169a
m32 <m32@m32.io>
2024-11-09 02:07:50 -0700

    pull everything into flake, generate a datafile for use in the zola build

--- flake.nix
+++ flake.nix
@@ -11,20 +11,67 @@
       systems = [ "x86_64-linux" "aarch64-linux" ];
 
       perSystem = { pkgs, self', ... }: {
-        devShells = {
-          default = (import ./shell.nix) {
-            inherit pkgs;
-            extraBuildInputs = [];
-          };
-        };
+        packages = with pkgs; rec {
+          # * The static website files as a package
+          static = stdenv.mkDerivation rec {
+            pname = "m32.io-static";
+            version = "${self.short-rev or self.dirtyShortRev}";
+            src = self;
 
-        packages = rec {
-          static = pkgs.callPackage ./default.nix {
-            inherit self pkgs;
+            buildInputs = with pkgs; [
+              zola
+              tailwindcss
+
+              # Perl
+              perl
+              perlPackages.GitRepository
+              perlPackages.Git
+              git
+              perlPackages.CPAN
+              perlPackages.RESTClient
+              perlPackages.YAML
+              perlPackages.DataDumper
+            ];
+
+            phases = [ "unpackPhase" "configurePhase" "buildPhase" "installPhase" ];
+
+            configurePhase = ''
+              export PATH="$PATH:${pkgs.lib.makeBinPath buildInputs}"
+
+              cp ${self'.packages.data-file} .nix-data.json
+            '';
+
+            buildPhase = ''
+              	           tailwindcss -i sass/style.scss -o static/styles/main.css
+                           zola build
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              cp -r ./public/* $out
+            '';
           };
+
+          data-file = writeText "nix-data.json"
+            (builtins.toJSON { inherit (self.sourceInfo) lastModified lastModifiedDate;
+                               rev = self.sourceInfo.rev or self.sourceInfo.dirtyRev;
+                               shortRev = self.sourceInfo.shortRev or self.sourceInfo.dirtyShortRev;
+                             });
 
           default = static;
         };
+
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [
+              rlwrap
+              perlPackages.PerlLanguageServer
+
+              xq
+              tree
+              git
+            ] ++ self'.packages.static.buildInputs;
+          };
       };
     };
 
